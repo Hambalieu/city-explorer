fd1ecc5bf3f7d4cdb1b40ffea378c059570a0eef
Hambalieu Jallow <hambalieujallow@gmail.com>
2022-01-23 04:50:40 -0800

    completed lab 8 front end

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import axios from 'axios';
-import  {Card, ListGroup }from 'react-bootstrap/';
+import { Card, ListGroup } from 'react-bootstrap';
+
+
 import './App.css';
-// import Weather from './Weather.js';
 
 class App extends React.Component {
   constructor(props) {
@@ -14,12 +15,14 @@ class App extends React.Component {
       renderError: false,
       errorMessage: '',
       weatherData: [],
-      showWeatherData: false
+      showWeatherData: false,
+      movieData: [],
+      showMovieData: false
     }
   }
 
   handleInput = e => this.setState({ searchQuery: e.target.value });
-
+  
 
   getCityInfo = async (e) => {
     e.preventDefault();
@@ -31,12 +34,6 @@ class App extends React.Component {
         cityData: cityResults.data[0],
         showMapandCityInfo: true
       })
-      let serverUrl = `${process.env.REACT_APP_SERVER_URL}/weather?searchQuery=${this.state.searchQuery}`;
-      let weatherResults = await axios.get(serverUrl);
-      this.setState({
-        weatherData: weatherResults.data,
-        showWeatherData: true
-      })
 
     } catch (error) {
       this.setState({
@@ -44,19 +41,56 @@ class App extends React.Component {
         errorMessage: `Error Occured: ${error.response.status}, ${error.response.data.error}`
       })
     }
-  };
+    this.getWeatherInfo();
+    this.getMoviesInfo();
+  }
+
+  getWeatherInfo = async () => {
+    let url = `${process.env.REACT_APP_SERVER_URL}/weather?lat=${Math.round(this.state.cityData.lat)}&lon=${Math.round(this.state.cityData.lon)}`;
+    let weatherResults = await axios.get(url);
+    this.setState({
+      weatherData: weatherResults.data,
+      showWeatherData: true
+    })
+
+  }
+
+  getMoviesInfo = async () => {
+    let url = `${process.env.REACT_APP_SERVER_URL}/movies?&location=${this.state.searchQuery}`;
+    let movieResults = await axios.get(url);
+    this.setState({
+      movieData: movieResults.data,
+      showMovieData: true
+
+    })
+
+
+  }
+
+
 
   render() {
-     let weatherToRender = this.state.weatherData.map((day, idx) => (
-          <ListGroup.Item key={idx}>
-          date: {day.date},High:{day.maxTemp},
-          Low:{day.lowTemp},{day.description}
-        </ListGroup.Item>
-    ));
-  
+    let weatherToRender = this.state.weatherData.map((day, idx) =>
+      <ListGroup.Item key={idx}>
+        Date: {day.date}, High: {day.maxTemp}, Low: {day.lowTemp},{day.description}
+      </ListGroup.Item>
+    )
+
+    let moviesToRender = this.state.movieData.map((movie, idx) =>
+      <ListGroup.Item key={idx}>
+        <Card>
+          <Card.Img
+            src ={movie.image_url}
+            alt ={movie.overview}>
+          </Card.Img>
+          <Card.Text>{movie.overview}</Card.Text>
+        </Card>
+      </ListGroup.Item>
+    
+    )
+
     return (
       <>
-      <div>
         <header>
           <h1>City Explorer</h1>
         </header>
@@ -69,18 +103,19 @@ class App extends React.Component {
             <button type="submit">Explore!</button>
           </form>
 
+          {this.state.renderError && <p>{this.state.errorMessage}</p>}
           {
             this.state.showMapandCityInfo &&
-            <article>
-              {this.state.renderError && <p>{this.state.errorMessage}</p>}
+            <article id='map'>
 
-              <Card border="dark" style={{ width: '80%' }} className="mapImg">
+              <Card border="dark" style={{ width: '60%' }} className="mapImg">
                 <Card.Img variant='top'
                   src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_ACCESS_TOKEN}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=11`}
                   alt='mapImage' />
                 <Card.Body>
                   <h1>{this.state.cityData.display_name}</h1>
-                  <p>Lat: {this.state.cityData.lat}, Lon:{this.state.cityData.lon}</p>
+                  <p>Lat: {this.state.cityData.lat}</p>
+                  <p>Lon:{this.state.cityData.lon}</p>
                 </Card.Body>
                 <Card.Footer>
                   <p>LocationIQ API &copy;hjallow </p>
@@ -88,16 +123,23 @@ class App extends React.Component {
 
               </Card>
             </article>}
-          <div>
-            {
-              this.state.showWeatherData &&
-              <ListGroup>
-                {weatherToRender}
-              </ListGroup>
-            }
-          </div>
+            <article>
+              {
+                this.state.showWeatherData &&
+                <ListGroup>
+                  {weatherToRender}
+                </ListGroup>
+              }
+            </article>
+            <article>
+              {
+                this.state.showMovieData &&
+                <ListGroup>
+                  {moviesToRender}
+                </ListGroup>
+              }
+            </article>
         </main>
-      </div> 
       </>
     );
   }
